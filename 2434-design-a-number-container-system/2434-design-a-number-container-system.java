4deb90017a29bc0eac963530d5f920df3b2eea08
Arnav Sharma <122526957+ARNAV-2005-SHARMA@users.noreply.github.com>
2025-02-08 17:53:03 +0530

    Time: 84 ms (47.95%), Space: 99.3 MB (45.21%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.java
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.java
@@ -1,30 +1,32 @@
+// Approach 1 -> Using Two map and one TreeSet
+// Time Complexity -> O(log n)
+// Space Complexity -> O(n)
 class NumberContainers {
-    Map<Integer, Integer> idx;
-    Map<Integer, TreeSet<Integer>> num;
+    Map<Integer, Integer> idx; // key -> index,  value -> number
+    Map<Integer, TreeSet<Integer>> num; // key -> number,  value -> sorted set of indices
     public NumberContainers() {
         num = new HashMap<>();
         idx = new HashMap<>();
     }
     
     public void change(int index, int number) {
-        
+        // If the index already exists, remove it from the previous number's set
         if(idx.containsKey(index)){
             int prevNum = idx.get(index);
-            num.get(prevNum).remove(index);
+            num.get(prevNum).remove(index); // O(log n)
 
             if(num.get(prevNum).isEmpty()){
-                num.remove(prevNum);
+                num.remove(prevNum); // Remove the number if no indices left (from num map)
             }
         }
-        idx.put(index, number);
-        num.putIfAbsent(number, new TreeSet<>());
-        num.get(number).add(index);
+        idx.put(index, number); // Update index to new number
+        num.putIfAbsent(number, new TreeSet<>()); // Add new number and new set in num map
+        num.get(number).add(index); // Add only index in treeSet in existing number
     }
     
-    public int find(int number) {
-        
+    public int find(int number) { // O(1)
         if(num.containsKey(number)){
-            return num.get(number).first();
+            return num.get(number).first(); // return the index from treeSet (first value)
         }
         return -1;
     }
