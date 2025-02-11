41a901d149751c9b7a03b86305c4891196ef07b8
Arnav Sharma <122526957+ARNAV-2005-SHARMA@users.noreply.github.com>
2025-02-11 15:23:29 +0530

    Time: 4 ms (49.85%), Space: 44.6 MB (37.21%) - LeetHub

---
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,34 @@
+// Approach 1 -> Brute Method (find and remove)
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+
+/*class Solution{
+    public String removeOccurrences(String s, String part){
+        while(s.contains(part)){ // Check if 'part' exists in 's'
+            s = s.replaceFirst(part, ""); // Remove the first occurrence of 'part'
+        }
+        return s;
+        
+    }
+}*/
+
+//-----------------------------------------------------------------------------------------------------------------------//
+// Approach 2 -> Using StringBuilder
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        StringBuilder sb = new StringBuilder();
+        int n = part.length();
+
+        for(char c : s.toCharArray()){
+            sb.append(c); // Append the character to StringBuilder
+
+            // Check if the last 'n' characters match 'part'
+            if(sb.length()>=n && sb.substring(sb.length() - n).equals(part)){
+                sb.delete(sb.length() - n, sb.length()); // Remove last occurrence of 'part'
+            }
+        }
+        return sb.toString();
+    }
+}
