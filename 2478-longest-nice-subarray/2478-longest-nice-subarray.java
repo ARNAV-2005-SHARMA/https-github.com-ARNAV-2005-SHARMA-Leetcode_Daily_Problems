5a8b283a848c07e1080024720c44a350ee6ec82a
Arnav Sharma <122526957+ARNAV-2005-SHARMA@users.noreply.github.com>
2025-03-19 00:41:37 +0530

    Time: 4 ms (28.34%), Space: 55.8 MB (57.77%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.java
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.java
@@ -1,3 +1,6 @@
+// Approach 1 -> Brute Force with bit manipulation
+// Time Complexity -> O(n^2)
+// Space Complexity -> O(1)
 class Solution {
     public int longestNiceSubarray(int[] nums) {
         int n = nums.length;
@@ -6,10 +9,10 @@ class Solution {
         for(int i=0;i<n;i++){
             int mask = 0;
             for(int j=i;j<n;j++){
-                if((nums[j] & mask) != 0){
-                    break;
+                if((nums[j] & mask) != 0){ 
+                    break; // if And not equal to zero then break the subarray
                 }
-                mask = mask | nums[j];
+                mask = mask | nums[j]; 
                 res = Math.max(res, j-i+1);
             }
         }
