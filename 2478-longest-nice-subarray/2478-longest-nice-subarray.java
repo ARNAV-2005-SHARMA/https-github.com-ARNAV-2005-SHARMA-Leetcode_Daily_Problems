b2864d93e5a5c911a36d5cda07bae8b898bc324f
Arnav Sharma <122526957+ARNAV-2005-SHARMA@users.noreply.github.com>
2025-03-19 11:34:58 +0530

    Time: 3 ms (98.67%), Space: 55.5 MB (77.45%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.java
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.java
@@ -1,7 +1,7 @@
 // Approach 1 -> Brute Force with bit manipulation
 // Time Complexity -> O(n^2)
 // Space Complexity -> O(1)
-class Solution {
+/*class Solution {
     public int longestNiceSubarray(int[] nums) {
         int n = nums.length;
         int res = 0;
@@ -15,6 +15,36 @@ class Solution {
                 mask = mask | nums[j]; 
                 res = Math.max(res, j-i+1);
             }
+        }
+        return res;
+    }
+}*/
+
+
+
+//------------------------------------------------------------------------------------------------------------------------//
+// Approach 2 -> Sliding Window Approach 
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+class Solution{
+    public int longestNiceSubarray(int[] nums){
+        int n = nums.length;
+        int i = 0;
+        int j = 0;
+
+        int res = 1;
+        int mask = 0;
+
+        while(j < n){
+            // Shrink the window if there is an overlap
+            while((nums[j] & mask)!=0){
+                mask = mask ^ nums[i]; // Remove the effect of nums[i]
+                i++;
+            }
+
+            res = Math.max(res, j-i+1); // store the max length
+            mask = mask | nums[j]; // Add nums[j] to the mask
+            j++; // increment pointer 
         }
         return res;
     }
